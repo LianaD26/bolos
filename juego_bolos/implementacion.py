43dfea3faebb2c7027353c169cb573200d6bb163
LianaD26 <limadio.uni@gmail.com>
2023-10-05 12:32:09 -0500

    third commit

--- juego_bolos/implementacion.py
+++ juego_bolos/implementacion.py
@@ -1,16 +1,27 @@
-class Juego:
-    def __init__(self):
-        self.fichas = []  # fichas
+from abc import ABC, abstractmethod
 
-    def puntaje(self):   # puntaje
+class ElementoJuego(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def puntaje(self):
+        pass
+
+class Juego(ElementoJuego):
+    def __init__(self):
+        super().__init__()
+        self.fichas = []
+
+    def puntaje(self):
         total = 0
         for ficha in self.fichas:
             total += ficha.puntaje()
         return total
 
-
-class Ficha:
+class Ficha(ElementoJuego):
     def __init__(self):
+        super().__init__()
         self.tiros = []
 
     def agregar_tiro(self, bolos):
@@ -20,19 +31,30 @@ class Ficha:
         total = 0
         for tiro in self.tiros:
             total += tiro.puntaje()
-
-        if self.es_spare(): # Si la ficha is_spare, se suma el puntaje del primer tiro de la siguiente ficha
-            siguiente_ficha = self.fichas[self.fichas.index(self) + 1]
-            total += siguiente_ficha.tiros[0].puntaje()
+        if self.es_spare():
+            total += self.siguiente_ficha().primer_tiro().puntaje()
         return total
 
     def es_spare(self):
         return len(self.tiros) == 2 and self.puntaje() == 10
 
+    def siguiente_ficha(self):
+        if self.juego() is not None:
+            indice_actual = self.juego().fichas.index(self)
+            if indice_actual < len(self.juego().fichas) - 1:
+                return self.juego().fichas[indice_actual + 1]
+        return None
 
-class Tiro:
+    def juego(self):
+        if isinstance(self, Juego):
+            return self
+        return None
+
+class Tiro(ElementoJuego):
     def __init__(self, bolos):
+        super().__init__()
         self.bolos = bolos
 
     def puntaje(self):
         return self.bolos
+
